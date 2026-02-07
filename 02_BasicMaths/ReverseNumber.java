2a18fb757fcbbc3391c657e82bba4560ad7ab588
Ashutosh <ashutoshjha6464@gmail.com>
2026-02-08 02:28:56 +0530

    reversed a signed 32 bit signed integer

--- 02_BasicMaths/ReverseNumber.java
+++ 02_BasicMaths/ReverseNumber.java
@@ -1,14 +1,20 @@
 public class ReverseNumber {
   public static void main(String[] args) {
-    int num = -456780;
+    int num = 1534236469;
     int sign = Integer.signum(num);
     num = sign * num;
     int reverseNumber = 0;
     while (num != 0) {
       int n = num%10;
       num = num/10;
+
+    if((reverseNumber > Integer.MAX_VALUE/10) || (reverseNumber < Integer.MIN_VALUE/10)) {
+        reverseNumber = 0;
+        break;
+      } 
       reverseNumber = (reverseNumber * 10) + n;
     }
+    
     System.out.println(sign*reverseNumber);
   }
 }
