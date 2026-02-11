26a6b03a32eea63b955d9e387d3e7dfd2462370f
Ashutosh <ashutoshjha6464@gmail.com>
2026-02-11 23:39:09 +0530

    basic recursion

---
+++ 03_BasicRecursion/printUsingRecursion.java
@@ -0,0 +1,22 @@
+public class printUsingRecursion {
+
+  public static void main(String[] args) {
+    // recursion function called
+    recFn(0);
+  }
+
+  public static void recFn(int count){
+
+    // base condition to avoid stack-overflow,
+    // we must have a base condition to stop 
+    // infinite recursion calls and run out of memory
+    if(count == 10)
+      // when condition is met function returns and 
+      // terminates all the functions waiting waiting in stack
+      return;
+    System.out.println(count - 1);
+
+    // function calls itself resulting into a recursion 
+    recFn(count + 1);
+  }
+}
