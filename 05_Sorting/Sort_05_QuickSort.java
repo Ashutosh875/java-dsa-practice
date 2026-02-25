d2d7bf2cdefd071c848c6ebada64f17795ec8859
Ashutosh <ashutoshjha6464@gmail.com>
2026-02-25 23:58:13 +0530

    Quick Sort

---
+++ 05_Sorting/Sort_05_QuickSort.java
@@ -0,0 +1,51 @@
+class QuickSort{
+
+  public void quickSort(int[] arr , int low , int high){
+    if(low < high){
+      int pivotIndex = getPivotIndex(arr , low , high);
+      quickSort(arr, low, pivotIndex-1);
+      quickSort(arr, pivotIndex+1 , high);
+    }
+  }
+
+  private int getPivotIndex(int[] arr , int low , int high){
+    int pivot = arr[low];
+    int i = low;
+    int j = high;
+
+    while(i<j){
+
+      while (arr[i] <= pivot && i <= high-1) {
+        i++;
+      }
+
+      while (arr[j] > pivot && j >= low+1) {
+        j--;
+      }
+
+      if(i<j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+      }
+
+    }
+
+    int temp = arr[low];
+    arr[low] = arr[j];
+    arr[j] = temp;
+    return j;
+  }
+}
+
+public class Sort_05_QuickSort {
+  public static void main(String[] args) {
+    
+    int[] arr = {3,2,1,2,4,1,5,2,3,1};
+    QuickSort qs = new QuickSort();
+    qs.quickSort(arr, 0, arr.length-1);
+    for(int i : arr){
+      System.out.print(i + " ");
+    }
+  }
+}
