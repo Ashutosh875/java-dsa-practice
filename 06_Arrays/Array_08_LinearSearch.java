96f15c81de191c06342ea95480306f2d7740d862
Ashutosh <ashutoshjha6464@gmail.com>
2026-03-04 22:20:08 +0530

    Linear Search

---
+++ 06_Arrays/Array_08_LinearSearch.java
@@ -0,0 +1,18 @@
+public class Array_08_LinearSearch {
+
+  public static int linearSearch(int[] nums , int k){
+    
+    for(int i=0; i<nums.length; i++){
+      if(nums[i] == k){
+        return i;
+      }
+    }
+    return -1;
+  }
+
+  public static void main(String[] args) {
+    
+    int index = linearSearch(new int[] {1,3,4,5,4,6,7,8} , 10);
+    System.out.println(index);
+  }
+}
