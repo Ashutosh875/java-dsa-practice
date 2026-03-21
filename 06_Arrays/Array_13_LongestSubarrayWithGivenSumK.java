1f8422b43a3e78cb862a6c39a2fdf3ea1289ad6b
Ashutosh <ashutoshjha6464@gmail.com>
2026-03-21 23:25:10 +0530

    Optimized Longest Subarray With Given Sum K

--- 06_Arrays/Array_13_LongestSubarrayWithGivenSumK.java
+++ 06_Arrays/Array_13_LongestSubarrayWithGivenSumK.java
@@ -1,3 +1,6 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Array_13_LongestSubarrayWithGivenSumK {
 
   public static int bruteLSubArrSumK(int[] nums , int k){
@@ -14,12 +17,41 @@ public class Array_13_LongestSubarrayWithGivenSumK {
 
     return len;
   }
+
+   public static int optLSubArrSumK(int[] nums , int k){
+
+    int sum = 0;
+    int len = 0;
+
+    Map<Integer,Integer> hash = new HashMap<>();
+    hash.put(0,1);
+
+    for(int i=0; i<nums.length; i++){
+
+      sum += nums[i];
+
+      int target = sum-k;
+
+      if(sum == k) { len = i + 1; }
+
+      if(hash.containsKey(target)) {len = Integer.max(len, i-hash.get(target));}
+
+      if(!hash.containsKey(target)) {hash.put(sum, i);}
+      
+    }
+
+    return len;
+    
+   }
+
   public static void main(String[] args) {
     
-    int[] arr = {1,3,2,1,1,2,3};
-    int k = 6;
+    int[] arr = {1,1,0,0,0};
+    int k = 0;
 
     System.out.println(bruteLSubArrSumK(arr, k));
+
+    System.out.println(optLSubArrSumK(arr, k));
     
   }
   
